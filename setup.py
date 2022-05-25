868004d0773fbec8c1334c03cefb7caa110af327
Yande <ndeye.ndiaye@nist.gov>
2022-05-25 09:48:45 -0400

    fix:upgrade version
    Change-Id: Ie4ac6650066de691035f1e3fa1e4ec3d2b9efed3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_visualization_insitu_app",
-    version="1.2.0",
+    version="1.2.1",
     description="Visualization Insitu app",
     long_description=long_desc,
     author="NIST IT Lab",
